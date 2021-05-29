b98db6e8eb6bfc92c1d3ed5d3be975d0e5f643fe
saribricka <saribricka44@gmail.com>
2021-05-29 13:09:39 +0200

    layout fixes

--- app/src/main/java/com/example/progetto/MainActivity.java
+++ app/src/main/java/com/example/progetto/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.log_in);
 
         //if there was no configuration change, create the fragment
         if (savedInstanceState == null)
