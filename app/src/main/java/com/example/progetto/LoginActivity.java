731a857271faa7e3216fe2884887c2f28e90703e
saribricka <saribricka44@gmail.com>
2021-06-07 15:54:28 +0200

    Login static method to simulate ready

--- app/src/main/java/com/example/progetto/LoginActivity.java
+++ app/src/main/java/com/example/progetto/LoginActivity.java
@@ -1,33 +1,88 @@
 package com.example.progetto;
 
+import android.accounts.AccountManager;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class LoginActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class LoginActivity<UserLoginTask> extends AppCompatActivity {
+
+    public static final String ARG_ACCOUNT_TYPE = "accountType";
+    public static final String ARG_AUTH_TOKEN_TYPE = "authTokenType";
+    public static final String ARG_IS_ADDING_NEW_ACCOUNT = "isAddingNewAccount";
+    public static final String PARAM_USER_PASSWORD = "password";
+
+    private AccountManager mAccountManager;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "sarabriccoli:helloworld", "simo19:ciao123", "penny:yorky", "duke:rescue2",
+            "saribricka:mobile21", "unibo:bolo", "catiapra:mobile21?", "chiacecca:unibo21!"
+    };
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    private String mUsername, mPassword;
+
+    //UI references
     private EditText username, password, reg_username, reg_password,
             reg_firstName, reg_lastName, reg_email, reg_birthday, reg_description;
     private Button login, signUp, reg_register;
     private TextInputLayout txtInLayoutUsername, txtInLayoutPassword, txtInLayoutRegPassword;
     private CheckBox rememberMe;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
 
+        mAccountManager = AccountManager.get(this);
+
+        //Set up the login form
+        mUsername = getIntent().getStringExtra(mAccountManager.KEY_ACCOUNT_NAME);
         username = findViewById(R.id.username);
+        username.setText(mUsername);
+
         password = findViewById(R.id.password);
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int id, KeyEvent event) {
+                if ( id == R.id.login || id == EditorInfo.IME_NULL){
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         login = findViewById(R.id.login);
         signUp = findViewById(R.id.signUp);
         txtInLayoutUsername = findViewById(R.id.txtInLayoutUsername);
@@ -37,7 +92,42 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mUsername = username.getText().toString();
+                mPassword = password.getText().toString();
+
+                LoginActivity.UserLoginTask userLoginTask = new LoginActivity.UserLoginTask(mUsername, mPassword);
+                userLoginTask.doInBackground();
+
+                attemptLogin();
                 ClickLogin();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success){
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class); //if login is successful, bring me to MainActivity class
+                                LoginActivity.this.startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(mUsername, mPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
         
@@ -47,6 +137,51 @@ public class LoginActivity extends AppCompatActivity {
                 ClickSignUp();
             }
         });
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        username.setError(null);
+        password.setError(null);
+
+        // Store values at the time of the login attempt.
+        mUsername = username.getText().toString();
+        mPassword = password.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password.
+        if (TextUtils.isEmpty(mPassword)) {
+            password.setError(getString(R.string.error_field_required));
+            focusView = password;
+            cancel = true;
+        } else if (mPassword.length() < 4) {
+            password.setError(getString(R.string.error_invalid_password));
+            focusView = password;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(mUsername)) {
+            username.setError(getString(R.string.error_field_required));
+            focusView = username;
+            cancel = true;
+        } else if (!mUsername.contains("@")) {
+            username.setError(getString(R.string.error_invalid_username));
+            focusView = username;
+            cancel = true;
+        }
+
     }
 
     private void ClickLogin() {
@@ -84,6 +219,14 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
     }
 
     private void ClickSignUp() {
@@ -153,5 +296,50 @@ public class LoginActivity extends AppCompatActivity {
         });
         
         dialog.show();
+    }
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        UserLoginTask(String email, String password) {
+            mUsername = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mUsername)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(mPassword);
+                }
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+
+            if (success) {
+                finish();
+                Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(myIntent);
+            } else {
+                password.setError(getString(R.string.error_invalid_password));
+                password.requestFocus();
+            }
+        }
     }
 }
